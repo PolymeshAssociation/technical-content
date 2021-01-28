7160aad4c7c0ccd8a1dd0339e6f4527724ad9052
Ibo Sy <ibo@b9lab.com>
2021-01-28 21:35:23 +0100

    add initial content structure

---
+++ content/01-introduction/structure.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const here = `${path.parse(__dirname).base}${path.sep}`;
+module.exports = [
+    `${here}introduction`,
+    `${here}why-polymesh`,
+    `${here}governance-on-polymesh`,
+    `${here}technical-stack`,
+    `${here}components-and-tools`,
+    `${here}polyx`,
+    `${here}identity`,
+    `${here}discover-nets`
+];
--- content/structure.js
+++ content/structure.js
@@ -1,3 +1,11 @@
 module.exports = {
-  "Features": require("./features/structure.js"),
+  "Introduction to Polymath": require("./01-introduction/structure.js"),
+  "Up and running in X minutes": require("./02-up-and-running/structure.js"),
+  "Originate an asset": require("./03-originate-an-asset/structure.js"),
+  "Distributing assets": require("./04-distributing-assets/structure.js"),
+  "Corporate actions": require("./05-corporate-actions/structure.js"),
+  "Settlement": require("./06-settlement/structure.js"),
+  "KYC in Polymesh": require("./07-kyc-in-polymesh/structure.js"),
+  "CDD": require("./08-cdd/structure.js"),
+  "Platform Features": require("./features/structure.js")
 };
