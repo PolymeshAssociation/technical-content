b55065365d14fbb297755d5afdecffeaf90ccbfc
Ibo Sy <ibo@b9lab.com>
2021-01-29 16:53:18 +0100

    update landingpage, assets, finalize

--- content/01-introduction/structure.js
+++ content/01-introduction/structure.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
-    `${here}introduction`,
     `${here}why-polymesh`,
     `${here}governance-on-polymesh`,
     `${here}technical-stack`,
