ffc7913152fbd9bd5f45d93ce5f88de9538cabbb
Ibo Sy <ibo@b9lab.com>
2021-02-04 16:38:08 +0100

    add missing font styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,8 @@
 import "@fontsource/inter"
+import "@fontsource/inter/700.css"
 import "@fontsource/ibm-plex-mono"
+import "@fontsource/ibm-plex-mono/400-italic.css"
+import "@fontsource/ibm-plex-mono/700.css"
 
 import "./src/css/layout.css";
 import "./src/css/temp-styles.css";
