1fb394194e040f256d3581cde60f6d28822361e6
3338e30b4a154caeafffecfe1a301a956c304862 <noreply@nowhere.com>
2021-02-05 16:16:27 -0700

    outline tweaks

--- content/01-introduction/structure.js
+++ content/01-introduction/structure.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
+	`${here}intro-introduction`,
     `${here}intro-why-polymesh`,
     `${here}intro-governance`,
     `${here}intro-stack`,
--- content/02-quickstart/structure.js
+++ content/02-quickstart/structure.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
-    `${here}quickstart-introduction`,
+    `${here}quickstart-introduction`,  
     `${here}quickstart-wallet`,
-    `${here}quickstart-cdd`,
+    `${here}quickstart-cdd`,      
     `${here}quickstart-local-node`,
+    `${here}quickstart-polyx`,     
     `${here}quickstart-quiz`
 ];
