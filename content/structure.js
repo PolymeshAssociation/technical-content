e4aed49e2a5c69e03d8918cdc63bcf4f08bdc82e
Xavier Leprêtre <xavier@b9lab.com>
2021-02-09 18:18:45 +0400

    Fleshed out a bit of settlement overview.

--- content/structure.js
+++ content/structure.js
@@ -3,7 +3,7 @@ module.exports = {
   "Quick Start": require("./02-quickstart/structure.js"),
   "Originate an asset": require("./03-originate/structure.js"),
   "Distribute assets": require("./04-distribute/structure.js"),
-  "Settlement": require("./06-settlement/structure.js"),  
+  "Settlement and Custody": require("./06-settlement/structure.js"),  
   "Corporate actions": require("./05-actions/structure.js"),
   "KYC in Polymesh": require("./07-kyc/structure.js"),
   "Customer Due Diligence (CDD)": require("./08-cdd/structure.js"),
