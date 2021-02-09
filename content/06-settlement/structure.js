f06258bb1bfff7c774842f5da85b7fc66525d81a
Ibo Sy <ibo@b9lab.com>
2021-02-09 16:05:28 +0100

    update module lps (wip)

--- content/06-settlement/structure.js
+++ content/06-settlement/structure.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
+		`${here}landingpage`,
     `${here}settlement-introduction`,
     `${here}settlement-dashboard`,
     `${here}settlement-sdk`,
