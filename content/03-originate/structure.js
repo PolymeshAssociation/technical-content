7261f854286ec22524f0854f377e013382fccb60
3338e30b4a154caeafffecfe1a301a956c304862 <noreply@nowhere.com>
2021-02-18 19:32:03 -0700

    added secondary keys page

--- content/03-originate/structure.js
+++ content/03-originate/structure.js
@@ -4,6 +4,7 @@ module.exports = [
     `${here}landingpage`,
     `${here}originate-introduction`,
     `${here}originate-dashboard`,
+    `${here}originate-secondary`,    
     `${here}originate-sdk`,
     `${here}originate-quiz`
 ];
