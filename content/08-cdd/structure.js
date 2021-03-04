e00fb7d87ec55069efcf26977a48dbb2442eb743
Xavier Leprêtre <xavier.lepretre@gmail.com>
2021-03-04 22:44:16 +0400

    Removed obsolete 08-cdd folder. Now in 07-kyc.

--- content/08-cdd/structure.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const here = `${path.parse(__dirname).base}${path.sep}`;
-module.exports = [
-    `${here}landingpage`,
-    `${here}cdd-introduction`,
-    `${here}cdd-dashboard`,
-    `${here}cdd-sdk`,
-    `${here}cdd-quiz`
-];
