a47b72daa0d004a58b47b1b95a211e88a8a600b3
Sam Schmeink <sam@saicu.de>
2021-03-18 16:41:31 +0100

    action cards

---
+++ src/components/ActionCard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import {breakpoints} from 'gatsby-theme-apollo-core';
+
+const Card = styled.a({
+  flex: "1",
+	position: "relative",
+	margin: "0 15px",
+  textDecoration: "none",
+	border: "1px solid #EDEDED",
+  borderRadius: "8px",
+  padding: "25px 20px",
+  marginBottom: "30px !important",
+  transition: "box-shadow 0.1s ease-in-out",
+  ":hover": {
+    boxShadow: "0px 0px 10px 2px rgba(100, 100, 100, .1)"
+  },
+  ":first-child": {
+    marginLeft: 0
+  },
+  ":last-child": {
+    marginRight: 0
+  },
+  [breakpoints.smMd]: {
+		display: "block",
+    margin: 0
+  }
+});
+
+const ActionCardLinkText = styled.div({
+  color: "#1348E3",
+	fontWeight: "700",
+	textDecoration: "none",
+  position: "relative",
+	bottom: 0,
+  marginBottom: "20px"
+});
+
+const ActionCardLinkImg = styled.img({
+  height: "inherit",
+	margin: "0 20px -2px",
+  display: "inline-block !important",
+  marginBottom: "-3px",
+  marginLeft: "10px",
+});
+
+const ActionCardLinkDesc = styled.p({
+  marginTop: "20px",
+	marginBottom: 0,
+	paddingBottom: 0,
+	color: "#6C7D88"
+});
+
+export default function ActionCard(props) {
+  return (
+    <Card href={props.link}>
+      <ActionCardLinkText>
+        {props.text}
+        <ActionCardLinkImg src="/icon-arrow-right.svg" class="action-icon"></ActionCardLinkImg>
+      </ActionCardLinkText>
+      <ActionCardLinkDesc>{props.desc}</ActionCardLinkDesc>
+    </Card>
+  );
+}
