7f958c153fbcc5694cc31b8c14f8d155b82e80d6
Ibo Sy <ibo@b9lab.com>
2021-03-30 06:10:19 +0200

    add glossary, update github pages

--- content/08-resources/structure.js
+++ content/08-resources/structure.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
-    `${here}github-repositories`
+    `${here}github-repositories`,
+    `${here}glossary`
 ];
