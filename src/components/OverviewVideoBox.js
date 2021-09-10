0819c7a6f2f7538449184f0e0f9c22444fac319e
Aman Alam <shekh.aman@gmail.com>
2021-09-10 00:12:31 -0400

    Revert "implements overview video component"
    This reverts commit b98a698f55a54a84906767a4441f360d5aee6702.

--- src/components/OverviewVideoBox.js
+++ src/components/OverviewVideoBox.js
@@ -11,7 +11,7 @@ const OverviewVideoBoxWrapper = styled.div({
     borderBottom: "1px solid #EDEDED",
     marginBottom: "40px",
     paddingBottom: "40px",
-	marginTop: "40px",
+    marginTop: "20px",
 });
 
 const ContentWrapper = styled.div({
